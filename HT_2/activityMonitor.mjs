2e201e12dd338df4a777b130ab73d170f3d1ee4b
Andrii Fedak <andrii_fedak@epam.com>
2023-08-30 14:58:37 +0300

    refactored

--- HT_2/activityMonitor.mjs
+++ HT_2/activityMonitor.mjs
@@ -4,7 +4,20 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { exec } from 'node:child_process'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const { command, parser } = getConfigBasedOnOS()
+const logActivity = logActivityInit()
+
+setInterval(() => {
+    exec(command, (_error, stdout, _stderr) => {
+        const { name, cpu, mem } = parser(stdout.trim().split(/\s+/));
+        const processInfo = `${cpu} ${mem} ${name}`
+
+        process.stdout.clearLine(0)
+        process.stdout.write(`\r${processInfo}`)
+
+        logActivity(processInfo)
+    });
+}, 100); // 10 times per 1s (1000 mls === 1s)
 
 function getConfigBasedOnOS() {
     const platform = os.platform();
@@ -29,30 +42,19 @@ function getConfigBasedOnOS() {
     }
 }
 
-function unixTimestamp(date = Date.now()) {
-    return Math.floor(date / 1000)
-}
+function logActivityInit() {
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const { command, parser } = getConfigBasedOnOS()
+    const logPeriod = 10 * 1000 // Once per minute program appends the output to the log file
+    let lastTimeLogged = Date.now() - logPeriod
 
-let logPeriod = 60 * 1000 // Once per minute program appends the output to the log file
-let lastTimeLogged = Date.now() - logPeriod
-function getMostCPUIntensiveProcess() {
-    exec(command, (_error, stdout, _stderr) => {
-        const line = stdout.trim().split(/\s+/);
-        const { name, cpu, mem } = parser(line)
-        const processInfo = `${cpu} ${mem} ${name}`
-        process.stdout.clearLine(0)
-        process.stdout.write(`\r${processInfo}`)
-
+    return function (processInfo) {
         const currentDate = Date.now()
         if (lastTimeLogged + logPeriod <= currentDate) {
             lastTimeLogged = currentDate
-            fs.appendFile(path.join(__dirname, 'activityMonitor.log'), `${unixTimestamp()}: ${processInfo}\n`, () => { })
+            const unixTimestamp = Math.floor(currentDate / 1000)
+            fs.appendFile(path.join(__dirname, 'activityMonitor.log'), `${unixTimestamp}: ${processInfo}\n`, () => { })
         }
-    });
+    }
 }
 
-setInterval(() => {
-    getMostCPUIntensiveProcess();
-}, 100); // 10 times per 1s (1000 mls === 1s)
